1bf8a5e20bd56c524f825bc8760c5f9346325104
Amy Shi <amyhshi92@gmail.com>
2022-03-02 11:30:09 -0500

    commit tree 48d38c039e37f1cd8aa459e4ca8567e870749389

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-document.addEventListener('DOMContentLoaded', () => {
+let clickedDog;
 
+document.addEventListener('DOMContentLoaded', () => {
+  loadDogs();
+
+  const form = document.querySelector("#dog-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(clickedDog);
+    fetch(`http://localhost:3000/dogs/${clickedDog}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        name: form.elements[0].value,
+        breed: form.elements[1].value,
+        sex: form.elements[2].value
+      })
+    })
+    .then(response => response.json())
+    .then(dog => console.log(dog));
+    location.reload();
+  })
 })
+
+function loadDogs() {
+  fetch("http://localhost:3000/dogs")
+  .then(response => response.json())
+  .then(dogs => dogs.forEach(dog => renderDog(dog)))
+}
+
+function renderDog(dog) {
+  const row = document.createElement("tr")
+  const dogName = document.createElement("td");
+  dogName.textContent = dog.name;
+  const dogBreed = document.createElement("td");
+  dogBreed.textContent = dog.breed;
+  const dogSex = document.createElement("td");
+  dogSex.textContent = dog.sex;
+  const buttonTd = document.createElement("td");
+  const button = document.createElement("button");
+  button.textContent = "Edit";
+  button.type = "button";
+  addEditFunctionality(button, dog);
+
+
+  buttonTd.append(button);
+  row.append(dogName, dogBreed, dogSex, buttonTd);
+  document.querySelector("#table-body").append(row);
+}
+
+function addEditFunctionality(button, dog) {
+  const form = document.querySelector("#dog-form");
+  button.addEventListener("click", () => {
+    form.elements[0].value = dog.name;
+    form.elements[1].value = dog.breed;
+    form.elements[2].value = dog.sex;
+    clickedDog = dog.id;
+  })
+}
